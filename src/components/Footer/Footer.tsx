306ae44e8f4a15addc8db3f1a104a2e57c879784
Geovanna Domingos <gmdomingosn@gmail.com>
2026-01-30 03:27:27 -0300

    adding footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from './Footer.module.scss';
+
+export function Footer() {
+    return (
+        <footer className={styles.footer}>STARSOFT © TODOS OS DIREITOS RESERVADOS</footer>
+    );
+}
